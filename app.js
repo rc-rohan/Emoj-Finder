98fc5dfea3a8a967d548d0acb1e7f39754bba7df
rc-rohan <rohan6hawat@gmail.com>
2020-09-24 23:28:29 +0530

    Added few basics HTML and CSS And Tested the API Working

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const apikey = "069cfd5b04b4a0fb85e0cc84dec7a18fd04c2238";
 const website = "https://emoji-api.com/";
+
+async function fetchStickers(){
+
+    const emoji = await fetch(
+            "https://emoji-api.com/emojis?access_key=069cfd5b04b4a0fb85e0cc84dec7a18fd04c2238"
+    );
+
+    const emojiResponse = await emoji.json();
+    console.log(emojiResponse);
+
+}
+
+fetchStickers();

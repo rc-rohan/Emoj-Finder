f0bf097563358a385e287e843dc4e782dc3fe441
rc-rohan <rohan6hawat@gmail.com>
2020-09-25 21:49:38 +0530

    Added Basic Styling and render the API data to screen

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-const apikey = "069cfd5b04b4a0fb85e0cc84dec7a18fd04c2238";
-const website = "https://emoji-api.com/";
-
-async function fetchStickers(){
-
-    const emoji = await fetch(
-            "https://emoji-api.com/emojis?access_key=069cfd5b04b4a0fb85e0cc84dec7a18fd04c2238"
-    );
-
-    const emojiResponse = await emoji.json();
-    console.log(emojiResponse);
-
-}
+const result = document.querySelector(".search-result");
 
 fetchStickers();
+async function fetchStickers() {
+  const emoji = await fetch(
+    "https://emoji-api.com/emojis?access_key=069cfd5b04b4a0fb85e0cc84dec7a18fd04c2238"
+  );
+
+  const emojiResponse = await emoji.json();
+  console.log(emojiResponse);
+  renderData(emojiResponse);
+}
+function renderData(response) {
+  let markup=``;
+  response.forEach((el)=>{
+     markup += `
+           <div class="emoji-details">
+             <div class="emoji-icon">${el.character}</div>
+             <div class="name">${el.unicodeName}</div>
+          </div>
+    `;
+  })
+  // for(let i=0;i<1793;i++){
+  //     markup += `
+  //         <div class="emoji-details">
+  //           <div class="emoji-icon">${response[i].character}</div>
+  //           <div class="name">${response[i].unicodeName}</div>
+  //         </div>
+  //     `;
+  // }
+  result.innerHTML = markup;
+}

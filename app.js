960ece8c27db993fb2a60539f591735c6b66bd14
rc-rohan <rohan6hawat@gmail.com>
2020-09-30 01:44:30 +0530

    Added modal with detials about emoji and removed alert error

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 /* implement the MVC architecture */
-/*
-  ! add the modal popup
-*/
 
-const result = document.querySelector(".search-result"),
+const searchResult = document.querySelector(".search-result"),
   inputField = document.querySelector(".search-input input"),
-  completeList = document.querySelector(".show-all-list");
+  completeList = document.querySelector(".show-all-list"),
+  modalElement = document.querySelector(".modal");
 
 var response = [];
 window.addEventListener("load", async () => {
@@ -18,10 +16,9 @@ window.addEventListener("load", async () => {
 inputField.addEventListener("keyup", () => {
   console.log(inputField.value);
   input = inputField.value;
-  if(input.length!==0){
+  if (input.length !== 0) {
     getSearchedEmoji(input);
-  }else{
-    /* when blank input field show only the face emoji list */
+  } else {
     getSearchedEmoji();
   }
 });
@@ -35,14 +32,14 @@ async function getAPIdata() {
   console.log(emojiResponse);
 }
 
-function getSearchedEmoji(input = "face") {
+function getSearchedEmoji(input = "smileys-emotion") {
   const filteredData = filterResponse(input, response);
   console.log(filteredData);
   if (filteredData.length === 0) {
-    console.log("alert");
     showAlert();
+  } else {
+    renderData(filteredData);
   }
-  renderData(filteredData);
 }
 
 function filterResponse(input, response) {
@@ -58,31 +55,26 @@ function filterResponse(input, response) {
   });
 }
 
-completeList.addEventListener("click",  () => {
-
-    /* display the entore respose list */
+completeList.addEventListener("click", () => {
   renderData(response);
-  console.log(response)
 });
 
-function clearAlert(){
-
-  const currAlert = document.querySelector('.alert-popup');
+function clearAlert() {
+  const currAlert = document.querySelector(".alert-popup");
   console.log(currAlert);
-  if(currAlert){
+  if (currAlert) {
     currAlert.remove();
   }
 }
 function showAlert() {
   clearAlert();
-  const alert = document.createElement('div');
-  alert.className = "alert-popup"
-  alert.innerHTML = "<p>&#x26A0; Username doesn't exists</p>";
-  const searchEl = document.querySelector(".search-input");
-  searchEl.insertAdjacentElement("afterbegin",alert);
-  setTimeout(clearAlert,1000);
+  const alert = document.createElement("div");
+  alert.className = "alert-popup";
+  alert.innerHTML = "<p>&#x26A0; Emoji doesn't exists</p>";
+  const alertDiv = document.querySelector(".alert");
+  alertDiv.insertAdjacentElement("afterbegin", alert);
+  setTimeout(clearAlert, 1000);
 }
-
 
 function renderData(response) {
   let markup = ``;
@@ -94,29 +86,40 @@ function renderData(response) {
           </div>
     `;
   });
-  result.innerHTML = markup;
+  searchResult.innerHTML = markup;
 }
 
-// result.addEventListener("click", (e) => {
-//   const element = e.target.closest(".emoji-details");
-//   const id = element.id;
-//   const data = response[id];
-//   console.log(data);
-//   const popup = document.querySelector(".modal");
-//   const markup = `
-//         <div class="popup">
-//             <div class="emoji-character">${data.character}</div>
-//             <hr>
-//             <div class="emoji-details">
-//                 <div class="emoji-name"><small>Name: </small><small>${data.unicodeName}</small></div>
-//                 <div class="emoji-code"><small>Code: </small><small>${data.codePoint}</small></div>
-//                 <div class="emoji-group"><small>Group: </small><small>${data.group}</small></div>
-//                 <div class="emoji-sub-group"><small>Sub-Group: </small><small>${data.subGroup}</small></div>
-//             </div>
-//         </div>
-//   `;
-//   popup.insertAdjacentHTML("afterbegin",markup);
-//   const bgColor = document.querySelector('.modal');
-//   bgColor.style.background = "rgba(86, 101, 115,.8   )";
-//   bgColor.style.height = "100vh";
-// });
+document.querySelector(".modal").addEventListener("click", (e) => {
+  const cancelBtn = e.target.closest(".cancel-btn");
+  if (cancelBtn) {
+    modalElement.innerHTML = "";
+    modalElement.style.height = "initial";
+    modalElement.style.width = "initial";
+    modalElement.style.background = "initial";
+  }
+});
+searchResult.addEventListener("click", (e) => {
+  const element = e.target.closest(".emoji-details");
+  if (element) {
+    const id = element.id;
+    const data = response[id];
+    const popup = document.querySelector(".modal");
+    const markup = `
+        <div class="popup">
+          <button class="cancel-btn">&#x2717;</button>
+            <div class="emoji-character">${data.character}</div>
+            <hr>
+            <div class="emoji-details">
+                <div class="emoji-name"><small>Name: </small><small>${data.unicodeName}</small></div>
+                <div class="emoji-code"><small>Code: </small><small>${data.codePoint}</small></div>
+                <div class="emoji-group"><small>Group: </small><small>${data.group}</small></div>
+                <div class="emoji-sub-group"><small>Sub-Group: </small><small>${data.subGroup}</small></div>
+            </div>
+        </div>
+  `;
+    popup.insertAdjacentHTML("afterbegin", markup);
+    modalElement.style.height = "100%";
+    modalElement.style.width = "100%";
+    modalElement.style.background = "rgba(66, 73, 73 ,0.5)";
+  }
+});
